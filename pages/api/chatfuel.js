8c657a8f5b9ff406e46ed659085f8fe37e514bd9
ronsigter <ronchester.mendoza@gmail.com>
2021-02-21 17:00:43 +0800

    Adds custom message

--- pages/api/chatfuel.js
+++ pages/api/chatfuel.js
@@ -2,7 +2,6 @@ import yyyymmdd from '../../utils/yyyymmdd'
 
 export default async (req, res) => {
   const date = yyyymmdd()
-
   const response = await fetch(
     `http://data.nba.net/10s/prod/v1/${date}/scoreboard.json`
   )
@@ -22,8 +21,21 @@ export default async (req, res) => {
 
   let text = ''
   formatedGames.map((game) => {
-    text = `HOME: ${game.home.name} \nAWAY: ${game.away.name} \n\n` + text
+    text = `${game.away.name} @ ${game.home.name}\n` + text
   })
+
+  let betDate = new Date()
+  betDate.setDate(new Date().getDate() + 1)
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+  text =
+    `${betDate.toLocaleDateString(undefined, options)}\n\n` +
+    text +
+    `\nClosing 7am`
 
   res.status(200).json({
     messages: [{ text }],
